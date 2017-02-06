9ddc3a7ff30e48dbd4150d6d2b47f0e54d672259
Shinmera <shinmera@tymoon.eu>
2017-02-06 16:36:49 +0100

    Always set SBCL_HOME envvar.

--- portacle.c
+++ portacle.c
@@ -141,7 +141,6 @@ int launch_git(char *root, int argc, char **argv){
 
 int launch_sbcl(char *root, int argc, char **argv){
   char path[PATHLEN]={0}, start[PATHLEN]={0};
-  if(!set_env("SBCL_HOME", pathcat(path, root, 5, PLATFORM, "sbcl", "lib", "sbcl", ""))) return 0;
 
   pathcat(start, root, 2, "config", "sbcl-init.lisp");  
   char *rargv[argc+3];
@@ -163,6 +162,7 @@ int configure_env(char *root){
 
   if(!set_env("ROOT", root)) return 0;
   if(!set_env("XDG_CONFIG_HOME", pathcat(path, root, 2, "config", ""))) return 0;
+  if(!set_env("SBCL_HOME", pathcat(path, root, 5, PLATFORM, "sbcl", "lib", "sbcl", ""))) return 0;
   if(!add_env("PATH", pathcat(path, root, 3, PLATFORM, "bin", ""))) return 0;
 #ifdef WIN
   if(!add_env("PATH", pathcat(path, root, 3, PLATFORM, "lib", ""))) return 0;
