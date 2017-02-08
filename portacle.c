4c500ffdf199eb3e6072b715d3ef3f99fa806889
Shinmera <shinmera@tymoon.eu>
2017-02-08 12:08:35 +0100

    LD_PRELOAD apparently still is being used on Windows and it does not like the default path! Make it linux-exclusive.

--- portacle.c
+++ portacle.c
@@ -135,7 +135,9 @@ int launch_emacs(char *root, int argc, char **argv){
 
 int launch_git(char *root, int argc, char **argv){
   char path[PATHLEN]={0};
+#ifdef LIN
   if(!set_env("LD_PRELOAD", pathcat(path, root, 3, PLATFORM, "launcher", "ld-wrap.so"))) return 0;
+#endif
   if(!add_env("PATH", pathcat(path, root, 5, PLATFORM, "git", "libexec", "git-core", ""))) return 0;
   
   pathcat(path, root, 4, PLATFORM, "git", "bin", "git");
