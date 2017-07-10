653a4b41f27d04fadb1733bd3cadb126fa66aa9c
Shinmera <shinmera@tymoon.eu>
2017-07-10 17:23:20 +0200

    Implement font registry on OS X.

--- src/portacle_mac.c
+++ src/portacle_mac.c
@@ -78,6 +78,44 @@ int launch(char *path, int argc, char **argv){
   return 1;
 }
 
+int run(int argc, ...){
+  char *argv[argc+1];
+  
+  va_list argp;
+  va_start(argp, argc);
+  for(int i=0; i<argc; ++i){
+    argv[i] = va_arg(argp, char *);
+  }
+  va_end(argp);
+  argv[argc] = 0;
+
+  pid_t pid = vfork();
+  if(pid < 0){
+    return 0;
+  }else if(pid == 0){
+    return execv(argv[0], argv);
+  }else{
+    int status;
+    waitpid(pid, &status, 0);
+    return !status;
+  }
+}
+
 int add_font(char *file){
-  return 0;
+  char *name = basename(file);
+  char *home = getenv("HOME");
+  char target[3000], fonts[3000];
+
+  strcpy(fonts, home);
+  strcat(fonts, "/Library/Fonts/");
+  strcpy(target, fonts);
+  strcat(target, name);
+  
+  if(access(target, F_OK) == -1){
+    if(!run(3, "/bin/mkdir", "-p", fonts))
+      return 0;
+    if(!run(4, "/bin/cp", "-p", file, target))
+      return 0;
+  }
+  return 1;
 }
